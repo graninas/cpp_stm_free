8938c39449d7851d242bce3690343374a68a46ed
Alexander Granin <graninas@gmail.com>
2018-03-10 14:38:43 +0700

    Tests restored.

--- cpp_stmTest/tst_stm.cpp
+++ cpp_stmTest/tst_stm.cpp
@@ -60,60 +60,60 @@ void STMTest::stupidGuidTest()
 
 void STMTest::stmlConstructionTest()
 {
-//    std::any any = std::any(10);
-//    stm::TVar<std::any> fakeTVar;
+    std::any any = std::any(10);
+    stm::TVar<std::any> fakeTVar;
 
-//    stm::NewTVarA<stm::TVar<std::any>> constr1 = stm::newTVarA(any);
-//    stm::WriteTVarA<fp::Unit>          constr2 = stm::writeTVarA(fakeTVar, any);
-//    stm::ReadTVarA<std::any>           constr3 = stm::readTVarA(fakeTVar);
+    stm::NewTVarA<stm::TVar<std::any>> constr1 = stm::newTVarA(any);
+    stm::WriteTVarA<fp::Unit>          constr2 = stm::writeTVarA(fakeTVar, any);
+    stm::ReadTVarA<std::any>           constr3 = stm::readTVarA(fakeTVar);
 
-//    stm::STMF<stm::TVar<std::any>> f1 { constr1 };
-//    stm::STMF<fp::Unit>            f2 { constr2 };
-//    stm::STMF<std::any>            f3 { constr3 };
+    stm::STMF<stm::TVar<std::any>> f1 { constr1 };
+    stm::STMF<fp::Unit>            f2 { constr2 };
+    stm::STMF<std::any>            f3 { constr3 };
 
-//    stm::STML<int> a1 = stm::pureF(10);
-//    stm::STML<stm::STMF<stm::TVar<std::any>>> a2 = stm::pureF(f1);
-//    stm::STML<stm::TVar<std::any>> a4 = stm::newTVar(10);
-//    stm::STML<std::any> a5 = stm::readTVar(fakeTVar);
-//    stm::STML<fp::Unit> a6 = stm::writeTVar(fakeTVar, 10);
+    stm::STML<int> a1 = stm::pureF(10);
+    stm::STML<stm::STMF<stm::TVar<std::any>>> a2 = stm::pureF(f1);
+    stm::STML<stm::TVar<std::any>> a4 = stm::newTVar(10);
+    stm::STML<std::any> a5 = stm::readTVar(fakeTVar);
+    stm::STML<fp::Unit> a6 = stm::writeTVar(fakeTVar, 10);
 }
 
 void STMTest::visitorTest()
 {
-//    std::any any = std::any(10);
-//    stm::TVar<std::any> fakeTVar;
+    std::any any = std::any(10);
+    stm::TVar<std::any> fakeTVar;
 
-//    stm::STML<int>      a1 = stm::pureF(10);
-//    stm::STML<stm::TVar<std::any>> a2 = stm::newTVar(10);
+    stm::STML<int>      a1 = stm::pureF(10);
+    stm::STML<stm::TVar<std::any>> a2 = stm::newTVar(10);
 
-//    stm::Context context1;
-//    auto ustamp1 = context1.newGUID();
-//    stm::AtomicRuntime runtime1(context1, ustamp1);
+    stm::Context context1;
+    auto ustamp1 = context1.newGUID();
+    stm::AtomicRuntime runtime1(context1, ustamp1);
 
-//    int result1 = stm::runSTML<int, stm::StmlVisitor>(runtime1, a1);
-//    QVERIFY(result1 == 10);
+    int result1 = stm::runSTML<int, stm::StmlVisitor>(runtime1, a1);
+    QVERIFY(result1 == 10);
 
-//    stm::Context context2;
-//    auto ustamp2 = context2.newGUID();
-//    stm::AtomicRuntime runtime2(context2, ustamp2);
-//    stm::TVar<std::any> result2 = stm::runSTML<stm::TVar<std::any>, stm::StmlVisitor>(runtime2, a2);
-//    QVERIFY(result2.id.size() == 32);
+    stm::Context context2;
+    auto ustamp2 = context2.newGUID();
+    stm::AtomicRuntime runtime2(context2, ustamp2);
+    stm::TVar<std::any> result2 = stm::runSTML<stm::TVar<std::any>, stm::StmlVisitor>(runtime2, a2);
+    QVERIFY(result2.id.size() == 32);
 }
 
 void STMTest::bind1Test()
 {
-//    std::function<stm::STML<std::string>(int)> f = [](int)
-//    {
-//        return stm::pureF(std::string("abc"));
-//    };
+    std::function<stm::STML<std::string>(int)> f = [](int)
+    {
+        return stm::pureF(std::string("abc"));
+    };
 
-//    stm::STML<std::string> s = stm::bind(stm::pureF(10), f);
+    stm::STML<std::string> s = stm::bind(stm::pureF(10), f);
 
-//    stm::Context context;
-//    stm::AtomicRuntime runtime(context, context.newGUID());
+    stm::Context context;
+    stm::AtomicRuntime runtime(context, context.newGUID());
 
-//    auto result = stm::runSTML<std::string, stm::StmlVisitor>(runtime, s);
-//    QVERIFY(result == "abc");
+    auto result = stm::runSTML<std::string, stm::StmlVisitor>(runtime, s);
+    QVERIFY(result == "abc");
 }
 
 void STMTest::bind2Test()
@@ -130,8 +130,8 @@ void STMTest::bind2Test()
     stm::Context context;
     stm::AtomicRuntime runtime(context, context.newGUID());
 
-    auto result = stm::runSTML<std::any, stm::StmlVisitor>(runtime, s);
-//    QVERIFY(result == "abc");
+    std::any result = stm::runSTML<std::any, stm::StmlVisitor>(runtime, s);
+    QVERIFY(std::any_cast<int>(result) == 10);
 }
 
 
