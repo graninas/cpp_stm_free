4be13d948946efd248add82d3ad2f1ee5c0f26e2
Alexander Granin <graninas@gmail.com>
2018-03-27 02:22:44 +0700

    New combinators.

--- cpp_stm/stm.h
+++ cpp_stm/stm.h
@@ -45,6 +45,15 @@ const auto writeTVarV = [](const auto& val)
     };
 };
 
+const auto modifyTVarV = [](const auto& tvar, const auto& f)
+{
+    auto m = free::readTVar(tvar);
+    return free::bind(m, [=](const auto& val)
+    {
+        return free::writeTVar(tvar, f(val));
+    } );
+};
+
 const auto retry = [](const auto&)
 {
     return free::retry<fp::Unit>();
@@ -60,6 +69,37 @@ STML<B> bind(const STML<A> ma, const free::ArrowFunc<A, B>& f)
 {
     return free::bind(ma, f);
 }
+
+template <typename A, typename B, typename Ret>
+STML<Ret> both(const STML<A>& ma,
+               const STML<B>& mb,
+               const std::function<Ret(A, B)>& f)
+{
+    return bind(ma, [=](const A& a){
+        return bind(mb, [=](const B& b){
+            return pure(f(a, b));
+        });
+    });
+}
+
+template <typename A, typename B>
+STML<fp::Unit>
+bothVoided(const STML<A>& ma,
+           const STML<B>& mb)
+{
+    return bind(ma, [&](const A&){
+        return bind(mb, [](const B&){
+            return pure(fp::unit);
+        });
+    });
+}
+
+const auto voided = [](const auto& ma)
+{
+    return bind(ma, [](const auto&) {
+        return pure(fp::unit);
+    });
+};
 
 } // namespace stm
 
